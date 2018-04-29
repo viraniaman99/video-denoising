beaece14e39be992ad24d8f4de98a130b0483a6c
Aman Virani <viraniaman@gmail.com>
2018-04-29 19:59:04 +0530

    added comments to omega mask file

--- get_omega_mask.py
+++ get_omega_mask.py
@@ -3,6 +3,17 @@ def get_omega_mask(noisy_mat, std_dev_mul):
 	# returns
 	# 	omega - mask denoting which elements to throw away
 
+	# parameters
+	# 	noisy_mat - the matrix obtained after patch matching and 
+	# 	median filtering
+	
+	# 	std_dev_mul - the factor with which to multiply the standard
+	# 	deviation to find the elements to throw away. For example, if 
+	# 	this parameter is set to 2, all elements of each row lying between
+	# 	mean-2*std_deviation to mean+2*std_deviation will be thrown away
+	# 	- which means the mask elements corresponding to these elements 
+	# 	will be set to 0
+
 	omega = np.array()
 
 	for row in noisy_mat:
